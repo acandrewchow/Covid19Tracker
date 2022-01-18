451fdcae52cba78823ad3fd66480035179094fe1
Andrew Chow <40132839+acandrewchow@users.noreply.github.com>
2022-01-17 22:35:37 -0500

    Update schools.py

--- School/schools.py
+++ School/schools.py
@@ -2,16 +2,10 @@
 
 '''
 schools.py
-  Author(s): Yash Pandya (1146824), Chibuzor Okwusiuno(1123349), Joshua Jones(1125364), Andrew Chow (1088114)
-
-  Earlier Contributors: Andrew Hamilton-Wright
-
-  Project: Lab Assignment 7 Script (Iteration 0)
-  Date of Last Update: Mar 30, 2021.
+  Author(s): Yash Pandya, Chibuzor Okwusiuno, Joshua Jones, Andrew Chow
 
   Functional Summary
-      schools demonstrates how we can
-      use school covid cases and testing data to analyze the current trends in Covid-19 daily
+      schools demonstrates how we can use school covid cases and testing data to analyze the current trends in Covid-19 daily
 
      Commandline Parameters: 1
         argv[0] = data file
